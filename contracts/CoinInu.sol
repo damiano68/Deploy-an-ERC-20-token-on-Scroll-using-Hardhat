e533c787b445f9d1c53a9b87194cd75735425996
damien schlienger <43938720+damiano68@users.noreply.github.com>
2024-02-14 12:26:04 +0100

    Update CoinInu.sol
    maybe seems better

--- contracts/CoinInu.sol
+++ contracts/CoinInu.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.8.4;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
-contract CoinInu is ERC20 {
+contract INUSCROLL is ERC20 {
     constructor() ERC20("Coin Inu", "CINU") {
         _mint(msg.sender, 1000000000 * 10 ** decimals());
     }
